e23c8d95f2b561b47272aedb49ba603780dc80fd
Suren <surenmkrtchyan03@gmail.com>
2024-04-16 12:30:04 +0400

    Fixed shift right algorithm

--- 2048Console/Program.cs
+++ 2048Console/Program.cs
@@ -1,6 +1,6 @@
 ﻿using _2048Game._2048Console.Classes;
 
-Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 2, 2, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 32, 2, 32 })));
 Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 4, 4, 0 })));
 Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 8, 0, 2 })));
 Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 8, 0, 0 })));
@@ -8,23 +8,29 @@ Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 8, 0, 0, 8 })));
 Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 4, 0, 0 })));
 Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 4, 8, 0 })));
 Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 0, 16, 16 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 16, 0, 16 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 0, 0, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 2, 0, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 0, 2, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 0, 0, 2 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 4, 2, 8, 2 })));
 
 int[] ShiftRight(int size, int[] array)
 {
-    for (int i = 0; i < size - 1; i++)
-    {
-        if (array[i] != 0 && array[i + 1] == 0)
-        {
-            array[i + 1] = array[i];
-            array[i] = 0;
-        }
+    int[] newArray = { 0, 0, 0, 0 };
+    int nonZeroIndex = size - 1;
 
-        if (array[i] == array[i + 1])
+    for (int i = size - 1; i >= 0; i--)
+    {
+        if (array[i] != 0 && nonZeroIndex < size -1 && array[i] == newArray[nonZeroIndex + 1])
         {
-            array[i + 1] *= 2;
-            array[i] = 0;
+            newArray[nonZeroIndex + 1] *= 2;
+        }
+        else if(array[i] != 0)
+        {
+            newArray[nonZeroIndex--] = array[i];
         }
     }
 
-    return array;
+    return newArray;
 }
