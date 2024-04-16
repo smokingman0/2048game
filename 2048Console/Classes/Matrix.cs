cda29e97250ae922dd7c0104faf889d6c9cee61e
Suren <surenmkrtchyan03@gmail.com>
2024-04-16 10:59:00 +0400

    Buggy version

---
+++ 2048Console/Classes/Matrix.cs
@@ -0,0 +1,101 @@
+﻿namespace _2048Game._2048Console.Classes;
+
+public class IntSquareMatrix
+{
+    public int Size { get; private set; }
+    private int[,] _Elements;
+
+    public int this[int i, int j]
+    {
+        get
+        {
+            if (i < 0 || i >= Size || j < 0 || j >= Size)
+                throw new IndexOutOfRangeException("Index out of range");
+
+            return _Elements[i, j];
+        }
+
+        set
+        {
+            if (i < 0 || i >= Size || j < 0 || j >= Size)
+                throw new IndexOutOfRangeException("Index out of range");
+
+            _Elements[i, j] = value;
+        }
+    }
+
+    public IntSquareMatrix(int size, int[,] elements)
+    {
+        Size = size;
+        _Elements = elements;
+    }
+
+    public IntSquareMatrix Transpose()
+    {
+        var transpose = Zeros(Size);
+
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                transpose[i, j] = this[j, i];
+            }
+        }
+
+        return transpose;
+    }
+
+    public static IntSquareMatrix Zeros(int size)
+    {
+        var elements = new int[size, size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                elements[i, j] = 0;
+            }
+        }
+
+        return new IntSquareMatrix(size, elements);
+    }
+
+    public override string ToString()
+    {
+        var matrixString = "";
+
+        for (int i = 0; i < Size; i++)
+        {
+            matrixString += "\n";
+
+            for (int j = 0; j < Size; j++)
+            {
+                matrixString += $"{this[i, j]} ";
+            }
+        }
+
+        return matrixString;
+    }
+
+    public static IntSquareMatrix Multiply(IntSquareMatrix A, IntSquareMatrix B)
+    {
+        if (A.Size != B.Size)
+        {
+            throw new ArithmeticException(
+                "Square matrixes A and B can't be multiplied, because their sizes are not equal");
+        }
+
+        var productMatrixSize = A.Size;
+        var product = Zeros(productMatrixSize);
+
+        for (int i = 0; i < productMatrixSize; i++)
+        {
+            for (int j = 0; j < productMatrixSize; j++)
+            {
+                product[i, j] += A[i, j] * B[j, i];
+            }
+        }
+
+        return product;
+    }
+}
---
+++ 2048Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using _2048Game._2048Console.Classes;
+
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 2, 2, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 4, 4, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 8, 0, 2 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 0, 8, 0, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 8, 0, 0, 8 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 4, 0, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 4, 8, 0 })));
+Console.WriteLine(string.Join(", ", ShiftRight(4, new int[] { 2, 0, 16, 16 })));
+
+int[] ShiftRight(int size, int[] array)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (array[i] != 0 && array[i + 1] == 0)
+        {
+            array[i + 1] = array[i];
+            array[i] = 0;
+        }
+
+        if (array[i] == array[i + 1])
+        {
+            array[i + 1] *= 2;
+            array[i] = 0;
+        }
+    }
+
+    return array;
+}
